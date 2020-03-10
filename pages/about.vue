702a73349bc6e043753065added8cf84c69f0050
maksmed <maxmed91@gmail.com>
2020-03-10 09:15:57 +0200

    commit

---
+++ pages/about.vue
@@ -0,0 +1,12 @@
+<template>
+    <section>
+        <h1>About Page</h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Perferendis esse repellendus sequi eum excepturi aliquid accusantium explicabo minima, voluptatem, assumenda, facere illo adipisci ut. Accusamus saepe sint quasi asperiores nostrum similique assumenda veniam, qui, ullam explicabo tempore molestiae aut. Molestias, magni? Amet tenetur ipsum possimus alias sunt, ipsam laborum numquam?</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Perferendis esse repellendus sequi eum excepturi aliquid accusantium explicabo minima, voluptatem, assumenda, facere illo adipisci ut. Accusamus saepe sint quasi asperiores nostrum similique assumenda veniam, qui, ullam explicabo tempore molestiae aut. Molestias, magni? Amet tenetur ipsum possimus alias sunt, ipsam laborum numquam?</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Perferendis esse repellendus sequi eum excepturi aliquid accusantium explicabo minima, voluptatem, assumenda, facere illo adipisci ut. Accusamus saepe sint quasi asperiores nostrum similique assumenda veniam, qui, ullam explicabo tempore molestiae aut. Molestias, magni? Amet tenetur ipsum possimus alias sunt, ipsam laborum numquam?</p>
+
+    </section>
+</template>
+
+<script>
+</script>
